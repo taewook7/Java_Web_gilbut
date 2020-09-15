88b6e644d6f873b8ce56cf44c39921ec0b67b1e6
taewook kim <x_o_d_n@naver.com>
2020-09-15 17:32:16 +0900

    Add files via upload

---
+++ pro05/src/sec01/ex01/ThirdServlet.java
@@ -0,0 +1,48 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ */
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L; 
+	//서블릿 클래스 직렬화를 위해 이클립스에서 자동으로 지정한 상수이니 삭제해도 무방하다.
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ThirdServlet init 메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("난 디스트로이");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet doGet 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet destroy 메서드 호출");
+	
+	}
+
+}
